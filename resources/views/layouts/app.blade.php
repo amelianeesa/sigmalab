540d17247afb4ea57e9daab9ea405b727b9a0381
nesyaahlm <nesyabella77@gmail.com>
2026-09-21 15:28:42 +0700

    last

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard - SIGMALAB Sucofindo</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/Logo_Suco_Nobg.png') }}?v=1">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-sucofindo.png') }}?v=1">
    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
