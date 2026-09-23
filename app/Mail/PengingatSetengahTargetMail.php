676298e5033da0c7a16dcec5138540a4fd6e4976
nesyaahlm <nesyabella77@gmail.com>
2026-09-23 09:31:09 +0700

    benerin create library

--- app/Mail/PengingatSetengahTargetMail.php
+++ app/Mail/PengingatSetengahTargetMail.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-
+ 
 class PengingatSetengahTargetMail extends Mailable
 {
     use Queueable, SerializesModels;
